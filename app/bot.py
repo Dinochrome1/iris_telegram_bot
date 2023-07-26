e15e9de11fcf1a86e4c65e74b712b9b84fb87149
Dmitry Drozdov <dinochrom1@gmail.com>
2023-07-26 14:42:25 +0400

    remove middleware from bot.py

--- app/bot.py
+++ app/bot.py
@@ -9,17 +9,11 @@ from config import config
 from handlers.admin import admin_router
 from handlers.echo import echo_router
 from handlers.user import user_router
-from misc.middlewares import ConfigMiddleware
 from misc.utils import on_startup, on_stop
 
 logger = logging.getLogger(__name__)
 log_level = logging.INFO
 bl.basic_colorized_config(level=log_level)
-
-
-def register_global_middlewares(dp: Dispatcher, config_):
-    dp.message.outer_middleware(ConfigMiddleware(config_))
-    dp.callback_query.outer_middleware(ConfigMiddleware(config_))
 
 
 async def main():
@@ -29,7 +23,6 @@ async def main():
     dp = Dispatcher(storage=storage)
     for router in [admin_router, user_router, echo_router]:
         dp.include_router(router)
-    register_global_middlewares(dp, config)
     dp.startup.register(on_startup)
     dp.shutdown.register(on_stop)
 
